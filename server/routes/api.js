684faf940ac6443f99b50fd45510ba612e69fb9d
Paul Greenham <paul.greenham@gmail.com>
2019-04-08 15:48:55 +0300

    set up server files and Expense mongo document template

---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express')
+// const request = require('request')
+const router = express.Router()
+
+
+const Expense = require('../models/Expense')
+
+
+module.exports = router
